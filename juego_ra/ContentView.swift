c126eebf67c87ccd87503e1d124a9d798cd27f2a
Jadzia Eleonore <raccoonevef@gmail.com>
2025-11-07 12:24:47 -0700

    [AGREGA] navegacion para calcular la distancia entre pistas

--- juego_ra/ContentView.swift
+++ juego_ra/ContentView.swift
@@ -12,13 +12,13 @@ struct ContentView: View {
     @State var servicio_ubicacion = ServicioUbicacion()
     
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("\(servicio_ubicacion.ubicacion_actual?.distance(from: pistas.first!.ubicacion))")
-            Text("Distancia a la pista es \(servicio_ubicacion.ubicacion_actual)")
-            //Text("\(servicio_ubicacion.ubicacion_actual?.longitude)")
+            
         }
         .padding()
     }
